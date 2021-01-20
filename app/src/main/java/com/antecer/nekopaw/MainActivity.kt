fe89d26f7a25f2fcbe835029d5e859f5f1db3da5
Antecer <antecer@outlook.com>
2021-01-20 16:35:52 +0800

    删除quack

--- app/src/main/java/com/antecer/nekopaw/MainActivity.kt
+++ app/src/main/java/com/antecer/nekopaw/MainActivity.kt
@@ -1,52 +1,51 @@
 package com.antecer.nekopaw
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import com.hippo.quickjs.android.*
-import com.koushikdutta.quack.QuackContext
 import de.prosiebensat1digital.oasisjsbridge.JsBridge
 import de.prosiebensat1digital.oasisjsbridge.JsBridgeConfig
 import de.prosiebensat1digital.oasisjsbridge.JsValue
+import kotlinx.coroutines.*
 
 
-class MainActivity : AppCompatActivity() {
-    class Foo{
-        fun print(s: Any) = println(s)
-    }
+class MainActivity : Activity(),
+    CoroutineScope by MainScope() {
+    @ExperimentalCoroutinesApi
+    @SuppressLint("LogNotTimber")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val js = assets.open("bechmarks.js").readBytes().decodeToString()
 
-        val quack = QuackContext.create()
-        quack.globalObject.set("Foo",Foo::class.java)
-        println(quack.evaluate("print = (...args) => new Foo().print([...args]);"))
-        println("-------------begin quack")
-        quack.evaluate(js)
-        println("-------------end quack")
 
-        val jsBridge = JsBridge(JsBridgeConfig.bareConfig())
-        JsValue.fromNativeFunction1(jsBridge) { s:Any -> println(s) }.assignToGlobal("print")
-        println("-------------begin JsBridge")
-        jsBridge.evaluateNoRetVal(js)
-        println("-------------end JsBridge")
+            val js = assets.open("bechmarks.js").readBytes().decodeToString()
 
-        val quickJS = QuickJS.Builder().build()
-        quickJS.createJSRuntime().use { runtime ->
-            runtime.createJSContext().use { context ->
-                // Create a JSFunction with a callback
-                val print: JSFunction =
-                    context.createJSFunction { context: JSContext, args: Array<JSValue> ->
-                        println(args)
-                        context.createJSUndefined()
-                    }
-                context.globalObject.setProperty("print", print)
-                println("-------------begin seven332")
-                context.evaluate(js, "test.js")
-                println("-------------end seven332")
-            }
-        }
+
+            Log.i("JS", "-------------begin JsBridge")
+
+            val result = "12345789"
+            val jsBridge = JsBridge(JsBridgeConfig.bareConfig())
+            // 测试1
+            JsValue.fromNativeValue(jsBridge, result).assignToGlobal("result")
+            JsValue.fromNativeFunction1(jsBridge) { s: Any -> Log.i("JS", s.toString()) }
+                .assignToGlobal("print")
+            JsValue.fromNativeFunction1(jsBridge) { s: String -> (s + "1") }
+                .assignToGlobal("test")
+            Log.i("JS", "-------------mid JsBridge")
+
+            jsBridge.evaluateNoRetVal("print(test('s'))")
+            jsBridge.evaluateNoRetVal("print(result)")
+            val msg: Deferred<Any> = jsBridge.evaluateAsync<Any>("result")
+            Log.i("JS", "-----JsBridge Get: $msg")
+
+//            val sum: Int = jsBridge.evaluate("1+2")
+//            Log.i("JsBridgeTest", sum.toString())
+//            Log.i("JsBridgePrint", "-------------begin JsBridge")
+            Log.i("JS", "-------------end JsBridge")
+
+
         // 调用原生方法的示例
         findViewById<TextView>(R.id.sample_text).text = (stringFromJNI())
     }
